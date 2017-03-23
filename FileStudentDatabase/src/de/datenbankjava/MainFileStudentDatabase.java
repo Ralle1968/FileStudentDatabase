d91e1493fe123390212243f2b2b8b5fecc9a9bc2
Ralle1968 <graef.ralf@gmail.com>
2017-03-23 21:53:28 +0100

    canges Main class

--- FileStudentDatabase/src/de/datenbankjava/MainFileStudentDatabase.java
+++ FileStudentDatabase/src/de/datenbankjava/MainFileStudentDatabase.java
@@ -4,7 +4,14 @@ public class MainFileStudentDatabase {
 
 	public static void main(String[] args) {
 		StudentDatabase sd = new FileStudentDatabase();
-
+		try {
+			String[] students = sd.readAllStudents();
+			for (String student : students) {
+				System.out.println(student);
+			}
+		} catch (StudentDatabaseException e) {
+			System.out.println("Es ist ein Fehler aufgetreten.");
+		}
 	}
 
 }
